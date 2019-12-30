82dd5adaeeab193b84bc5b639ce3d0a99596f0d1
riteshprasad1 <riteshsprasad@gmail.com>
2019-12-30 15:34:20 +0530

    Added Java program and class file

---
+++ FirstProg.java
@@ -0,0 +1,15 @@
+
+public class FirstProg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a[] = {10,20,30};
+		
+		for(int item:a)
+			System.out.println("Number is"+item);
+		
+
+	}
+
+}
