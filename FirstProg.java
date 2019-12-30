246c7b21bbab6cf97ca1c9296f8c53f922c7491c
riteshprasad1 <riteshsprasad@gmail.com>
2019-12-30 16:21:37 +0530

    Modified numbers

--- FirstProg.java
+++ FirstProg.java
@@ -4,7 +4,7 @@ public class FirstProg {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int a[] = {10,20,30};
+		int a[] = {20,40,60};
 		
 		for(int item:a)
 			System.out.println("Number is"+item);
